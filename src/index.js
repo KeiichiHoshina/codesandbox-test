2c3b54949032e99a6415d95b89872068e8d4e8a4
Keiichi Hoshina <masayukihoshina3@gmail.com>
2021-08-01 23:50:41 +0900

    23. スプレッド構文 ...

--- src/index.js
+++ src/index.js
@@ -1,5 +1,26 @@
-// 22. デフォルト値 =
+// 23. スプレッド構文 ...
 
-const sayHello = (name = "ゲスト") => console.log(`こんにちは${name}さん！`);
-sayHello();
-sayHello("じゃけぇ");
+// const arr1 = [1, 2];
+// console.log(arr1);
+// console.log(...arr1);
+
+// const sumFunc = (num1, num2) => console.log(num1 + num2);
+// sumFunc(arr1[0], arr1[1]);
+// sumFunc(...arr1);
+
+// const arr2 = [1, 2, 3, 4, 5];
+// const [num1, num2, ...num3] = arr2;
+// console.log(num1);
+// console.log(num2);
+// console.log(num3);
+
+// const arr4 = [10, 20];
+// const arr5 = [30, 40];
+
+// const arr6 = [...arr4];
+// arr6[0] = 100;
+// console.log(arr6);
+// console.log(arr4);
+
+// const arr7 = [...arr4, ...arr5];
+// console.log(arr7);
