d8146132f0446cdee09f3cf7bed103497cac4c2a
Keiichi Hoshina <masayukihoshina3@gmail.com>
2021-08-02 00:45:27 +0900

    25. 三項演算子 ? :

--- src/index.js
+++ src/index.js
@@ -1,32 +1,14 @@
-// 24. mapやfilterを使った配列の処理
+// 25. 三項演算子 ? :
 
-// const nameArr = ["田中", "山田", "じゃけぇ"];
+// const val1 = 1 > 0 ? 'trueです' : 'falseです';
+// console.log(val1);
 
-// for (let i = 0; i < nameArr.length; i++) {
-//   console.log(nameArr[i]);
-//   console.log(`${i+1}番目は${nameArr[i]}です`);
-// }
+// const num = 1300;
+// const formatteNum =
+//   typeof num === "number" ? num.toLocaleString() : "数値を入力してください";
+// console.log(formatteNum);
 
-// const nameArr2 = nameArr.map((name) => {
-//   return name;
-// });
-// console.log(nameArr2);
-
-// nameArr.map((name) => console.log(name));
-// nameArr.map((name, i) => console.log(`${i + 1}番目は${name}です`));
-// nameArr.map((name) => console.log(`${name}です`));
-
-// const numArr = [1, 2, 3, 4, 5];
-// const newNumArr = numArr.filter((num) => {
-//   return num % 2 === 1;
-// });
-// console.log(newNumArr);
-
-// const newNameArr = nameArr.map((name) => {
-//   if (name === "じゃけぇ") {
-//     return name;
-//   } else {
-//     return `${name}さん`;
-//   }
-// });
-// console.log(newNameArr);
+// const checkSum = (num1, num2) => {
+//   return num1 + num2 > 100 ? "100を超えています！！" : "許容範囲ないです";
+// };
+// console.log(checkSum(50, 60));
