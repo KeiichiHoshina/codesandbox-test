eb7508c395a44217d5e3bf2256d7d0414a12e8aa
Keiichi Hoshina <masayukihoshina3@gmail.com>
2021-08-01 23:29:57 +0900

    22. デフォルト値 =

--- src/index.js
+++ src/index.js
@@ -1,22 +1,5 @@
-// 21. 分割代入 {}
+// 22. デフォルト値 =
 
-// const myProfile = {
-//   name: "じゃけぇ",
-//   age: 28
-// };
-
-// const message1 = `名前は${myProfile.name}です。年齢は${myProfile.age}歳です。`;
-// console.log(message1);
-
-// const { name, age } = myProfile;
-// const message2 = `名前は${name}です。年齢は${age}歳です。`;
-// console.log(message2);
-
-// const myProfile = ["じゃけぇ", 28];
-
-// const message3 = `名前は${myProfile[0]}です。年齢は${myProfile[1]}歳です。`;
-// console.log(message3);
-
-// const [name, age] = myProfile;
-// const message4 = `名前は${name}です。年齢は${age}歳です。`;
-// console.log(message4);
+const sayHello = (name = "ゲスト") => console.log(`こんにちは${name}さん！`);
+sayHello();
+sayHello("じゃけぇ");
