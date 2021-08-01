33aa32d63748e7d8501366051b2241467c898033
Keiichi Hoshina <masayukihoshina3@gmail.com>
2021-08-01 22:43:17 +0900

    18. const、let等の変数宣言

--- src/index.js
+++ src/index.js
@@ -1,10 +1,32 @@
-import "./styles.css";
+// 18. const、let等の変数宣言
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// let val2 = "let変数";
+// console.log(val2);
+
+// val2 = "let変数上書き";
+// console.log(val2);
+
+// let val2 = "let変数際宣言";
+// console.log(val2);
+
+// const val3 = "const変数";
+// console.log(val3);
+
+// val3 = "const変数";
+// console.log(val3);
+
+// const val3 = "const変数";
+// console.log(val3);
+
+// const val4 = {
+//   name: "じゃけぇ",
+//   age: 28
+// };
+// val4.name = "jak";
+// val4.address = "Hiroshima";
+// console.log(val4);
+
+// const val5 = ["dog", "cat"];
+// val5[0] = "bird";
+// val5.push("monkey");
+// console.log(val5);
